d8b8fc3f6d1fcd06478af23e1bc9b5c87cd39ea0
Patrick ten Brinke <33376476+mrliber8@users.noreply.github.com>
2022-11-25 09:15:31 +0100

    Nu opent hij automatisch de IIS Web-Server in een browser als je het script runt

--- IIS.ps1
+++ IIS.ps1
@@ -4,3 +4,13 @@ if ($srv.installed) {#Do Nothing, if statement staat erin voor het testen zodat 
 } else {
     Install-WindowsFeature -name Web-Server -IncludeManagementTools #Installeer de IIS Web-Server
 }
+
+#De IIS Web-Server kan benaderd worden op het IP_Address van de server
+#Krijg het IP-Address en sla deze op in een variabele
+$SIPAddress = (Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Ethernet0).IPAddress
+
+
+#Open de url van de webserver in firefox
+#Voor Edge moet je Firefox vervangen door msedge
+#Voor Chrome moet je Firefox vervangen door chrome
+[system.Diagnostics.Process]::Start("firefox","$SIPAddress")
