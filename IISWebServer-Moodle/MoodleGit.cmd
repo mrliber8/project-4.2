d145d940a9a19267e5dbebdb6c0ba8fd965f1fac
Patrick ten Brinke <33376476+mrliber8@users.noreply.github.com>
2022-11-29 09:57:30 +0100

    committed

--- IISWebServer-Moodle/MoodleGit.cmd
+++ IISWebServer-Moodle/MoodleGit.cmd
@@ -3,16 +3,16 @@ REM Ga naar de c schijf
 cd C:\inetpub\wwwroot
 
 REM Clone Moodle
-git clone git://git.moodle.org/moodle.git
+"C:\Program Files\Git\cmd\git.exe" clone git://git.moodle.org/moodle.git
 
 REM Ga naar de moodle directory
 cd moodle
 
 REM Maak een branch genaamd MOODLE_400_STABLE wat de laatste stable moodle versie is
-git branch --track MOODLE_400_STABLE origin/MOODLE_400_STABLE
+"C:\Program Files\Git\cmd\git.exe" branch --track MOODLE_400_STABLE origin/MOODLE_400_STABLE
 
 REM Switch naar de nieuwe branch
-git checkout MOODLE_400_STABLE 
+"C:\Program Files\Git\cmd\git.exe" checkout MOODLE_400_STABLE 
 
 REM Pauze houd de window open maar staat er alleen in voor testen.
 PAUSE
